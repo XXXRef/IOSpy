f3d73b0c9e74249b264f51b6397c445f827381cb
XXXRef <h1deandseek@yandex.com>
2020-04-19 20:24:54 +0300

    Impletented install scripts

--- install_stuff/INSTALL.bat
+++ install_stuff/INSTALL.bat
@@ -1,17 +1,17 @@
-SET regInitFilePath="iospy_reg_init.reg"
-SET driverInstallFilePath="IOSpy.inf"
+SET regInitFilePath=%CD%\iospy_reg_init.reg
+SET driverInstallFilePath=%CD%\..\BUILD\IOSpy.inf
 
 echo "IOSpy installer script"
 echo ""
 
 :: Preparing registry
 echo "Injecting needed stuff into registry..."
-%regInitFilePath%
+regedit %regInitFilePath%
 echo "Done"
 
 :: Install driver as service
 echo "Installing driver..."
-%driverInstallFilePath%
+%SystemRoot%\System32\InfDefaultInstall.exe %driverInstallFilePath%
 echo "Done"
 
 
